bb78ce4df11cb4c6990e3563a6acb9c5ec0d1746
Ratnesh Maurya <rmaurya@initializ.io>
2023-09-08 17:10:16 +0530

    final

--- New folder/news_all_api_with_database.py
+++ New folder/news_all_api_with_database.py
@@ -1,72 +1,72 @@
-
-from flask import Flask,jsonify
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from bson import ObjectId
 from bson.json_util import dumps
-from pymongo import MongoClient
-from bson import json_util
 import json
-import requests
-                                                 
-app=Flask(__name__)
 
-#connect to mongodb
-client = MongoClient('localhost', 27017)
+import pymongo
 
-#connect to database
+app = Flask(__name__)
+
+# connect to mongodb
+connection_string = "mongodb+srv://ratnesh:ratnesh@cluster0.3ka0uom.mongodb.net/"
+
+# Establish a connection to the MongoDB Atlas cluster
+client = pymongo.MongoClient(connection_string)
+
+
+# connect to database
 db = client['news_database']
 
-#connect to collection
+# connect to collection
 news = db['news_collection']
 crime_news = db['crime_news_collection']
-sports_news = db['sports_news_collection'] 
+sports_news = db['sports_news_collection']
+
+
+# Convert ObjectId to string in JSON response
+def jsonify_with_objectid(data):
+    return jsonify(json.loads(dumps(data, default=str)))
+
 
 # Get all news articles
-@app.route('/news',methods=['GET'])
-def home():
-    return jsonify(dumps(news.find()))
-   
+@app.route('/news', methods=['GET'])
+def get_all_news():
+    return jsonify_with_objectid(news.find())
 
-@app.route('/news/<int:count>',methods=['GET'])
 
+# Get a specific number of news articles
+@app.route('/news/<int:count>', methods=['GET'])
 def get_news_count(count):
-    if count<=news.count():
-        response_news= [news.find()[i] for i in range(count) ]
-        return jsonify({'news':response_news})
-    else:
-        return jsonify({'message':'Not enough news articles available'})
-    
-    
+    news_list = list(news.find().limit(count))
+    return jsonify_with_objectid(news_list)
+
 
 # Get all crime news articles
 @app.route('/news/crime', methods=['GET'])
-def get_crime_news():
-    return dumps(crime_news.find())
+def get_all_crime_news():
+    return jsonify_with_objectid(crime_news.find())
 
-@app.route('/news/crime/<int:count>',methods=['GET'])
 
+# Get a specific number of crime news articles
+@app.route('/news/crime/<int:count>', methods=['GET'])
 def get_crime_news_count(count):
-    if count<=crime_news.count():
-        response_news= [crime_news.find()[i] for i in range(count) ]
-        return jsonify({'news':response_news})
-    else:
-        return jsonify({'message':'Not enough crime news articles available'})
-    
+    crime_news_list = list(crime_news.find().limit(count))
+    return jsonify_with_objectid(crime_news_list)
+
 
 # Get all sports news articles
 @app.route('/news/sports', methods=['GET'])
-def get_sports_news():
-    return dumps(sports_news.find())
+def get_all_sports_news():
+    return jsonify_with_objectid(sports_news.find())
 
-@app.route('/news/sports/<int:count>',methods=['GET'])
 
+# Get a specific number of sports news articles
+@app.route('/news/sports/<int:count>', methods=['GET'])
 def get_sports_news_count(count):
-    if count<=sports_news.count():
-        response_news= [sports_news.find()[i] for i in range(count) ]
-        return jsonify({'news':response_news})
-    else:
-        return jsonify({'message':'Not enough sports news articles available'})
+    sports_news_list = list(sports_news.find().limit(count))
+    return jsonify_with_objectid(sports_news_list)
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
